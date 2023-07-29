e86e78144ce6c9177d4d6b8f131a63e067a0fe89
unknown <mandalsalman28818@gmail.com>
2023-07-29 09:10:55 +0530

    cookie problem on production

--- Server/utils/jwtToken.js
+++ Server/utils/jwtToken.js
@@ -1,12 +1,16 @@
 exports.sendToken = (user, res, statusCode) => {
     const COOKIE_EXPIRE = 5;
     const token = user.getJWTToken();
+
     const options = {
         httpOnly: true,
-        expires: new Date(Date.now() + COOKIE_EXPIRE * 24 * 60 * 60 * 1000)
+        expires: new Date(Date.now() + COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        sameSite: "none",
+        secure: true,
     };
+
     res.status(statusCode).cookie("token", token, options).json({
-        success:true,
+        success: true,
         user,
         token
     });
